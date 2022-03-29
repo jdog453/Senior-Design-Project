50a07a16844370fd344024560582b0d028fb87da
Bbot64 <45585560+Bbot64@users.noreply.github.com>
2022-03-29 16:42:30 -0400

    updates

--- senior_project_facial_recognition/facial_recognition/utilities/alignFaces.py
+++ senior_project_facial_recognition/facial_recognition/utilities/alignFaces.py
@@ -12,7 +12,7 @@ class AlignFaces:
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor("../../facial_detection/models/shape_predictor_68_face_landmarks.dat")
 
-    def align_faces(self, image, imagePath = None):
+    def align_faces(self, image):
         fa = FaceAligner(self.predictor, desiredFaceWidth=256)
         # load the input image, resize it, and convert it to grayscale
         image = imutils.resize(image, width=800)
@@ -20,9 +20,11 @@ class AlignFaces:
         # A list of rectangle elements are returned. Each rectangle represents the coordinates of a bounding box around
         # a face that was detected in the image.
         rects = self.detector(gray, 2)
-        # We verify that at least one face is detected within the image.
-        if len(rects) > 0:
+        if len(rects) > 0: # check that at least one face is detected within the image.
+            if len(rects) > 1:
+                print("[INFO] multiple detections in image being aligned.")
             # The first bounding box is aligned and the face is returned.
             return fa.align(image, gray, rects[0])
         else:
-            return None
+            print("[INFO] no detections found in image. Returning original.")
+            return image # if no detections were found, then the original image is returned
