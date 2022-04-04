57e9b4b5af7bfb753e273ecba52a37dbbe0b410f
Bbot64 <45585560+Bbot64@users.noreply.github.com>
2022-04-04 14:24:01 -0400

    updates

--- senior_project_facial_recognition/facial_recognition/utilities/alignFaces.py
+++ senior_project_facial_recognition/facial_recognition/utilities/alignFaces.py
@@ -11,20 +11,23 @@ class AlignFaces:
         # The detector and predictor are initialized.
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor("../../facial_detection/models/shape_predictor_68_face_landmarks.dat")
+        self.fa = FaceAligner(self.predictor, desiredFaceWidth=256)
 
     def align_faces(self, image):
-        fa = FaceAligner(self.predictor, desiredFaceWidth=256)
-        # load the input image, resize it, and convert it to grayscale
-        image = imutils.resize(image, width=800)
+        # The input imageis loaded, resized, and converted to grayscale
+        image = imutils.resize(image, width=750)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # A list of rectangle elements are returned. Each rectangle represents the coordinates of a bounding box around
-        # a face that was detected in the image.
+        
+        # Each rect represents the coordinates of a detected face's bounding box.
         rects = self.detector(gray, 2)
-        if len(rects) > 0: # check that at least one face is detected within the image.
-            if len(rects) > 1:
-                print("[INFO] multiple detections in image being aligned.")
-            # The first bounding box is aligned and the face is returned.
-            return fa.align(image, gray, rects[0])
-        else:
-            print("[INFO] no detections found in image. Returning original.")
-            return image # if no detections were found, then the original image is returned
+        
+        if len(rects) > 1:
+            print("[INFO] multiple detections in image being aligned.")
+            
+        for rect in rects:
+            # Only the first detection is returned.
+            return self.fa.align(image, gray, rect)
+        
+        # If there were no detections, then the original image is resized and returned.
+        print("[INFO] no detections found in image. Returning original.")
+        return imutils.resize(image, width=256)
